c15385bd31fae1e457b59b709701b38f8dce3888
AlexandraAvasiloae <alexandra.avasiloae28@gmail.com>
2021-02-21 15:22:18 +0200

    optional

--- src/Optional/SecondJava.java
+++ src/Optional/SecondJava.java
@@ -3,24 +3,28 @@ package Optional;
 import java.io.*;
 import java.util.*;
 public class SecondJava {
+
     public static void main(String args[]){
-        if(args.length<1){ //validez argumentul
-            //System.err.println(-1);
-            System.out.println("Nu s-a specificat niciun argument la linia de comanda");
+
+        /*argument validation*/
+        if(args.length<1){
+            System.err.println(-1);
+            System.out.println("Specify the argument!");
         }
-        int n =Integer.parseInt(args[0]); //transform argumentul in nr intreg
+
+        /*convert the argument to int*/
+        int n =Integer.parseInt(args[0]);
+
+        /*generate the random graph by filling the adjacency matrix with random values of 0 or 1*/
         int matrix[][] = new int[n][n];
-        System.out.println(n);
-        /*creez un graf random prin generarea unei matrici de adiacenta random pentru nr. de varfuri precizat la linia de comanda (adica n)*/
         int i,j;
         for(i=0;i<n;i++){
             for(j=0;j<n;j++){
                 matrix[i][j]=(int) (Math.random() * 10)%2;
             }
         }
-        /*afisarea matricii*/
+        /*Display on the screen the generated matrix*/
         for (int[] row : matrix){
-            // converteste fiecare rand la string si afiseaza fiecare rand separat
                 System.out.println(Arrays.toString(row));
             }
     }
